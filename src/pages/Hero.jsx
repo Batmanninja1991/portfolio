85cd9eb57ced3d51db968b97045990cd9d991a2c
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-06-13 13:08:02 -0700

    text size

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="w-3/4 flex flex-col justify-center gap-20 text-left">
         <div>
           <p className="text-base text-white">Hi, my name is</p>
-          <h1 className="pl-4 text-6xl text-blue-600">David Shaw.</h1>
+          <h1 className="pl-4 text-5xl text-blue-600">David Shaw.</h1>
           <p className="pl-4 text-2xl leading-loose text-blue-300">A Full Stack Web Developer</p>
           <p className="text-base leading-tight text-white">
             My current focus is on developing user-friendly software that
