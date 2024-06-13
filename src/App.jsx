8f38411e7b59c08d9afa964da3bee02401650f44
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-06-13 13:15:49 -0700

    link update 2

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/portfolio" element={<Hero />} />
+        <Route path="/" element={<Hero />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
